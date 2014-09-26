182cf404965cf6d8118ac810b58d2fb2d13beb49
Gaël Demette <gael.demette@webtales.fr>
2014-09-26 16:23:34 +0200

    + Namespace + Base def

--- src/RubedoDoc/Rest/V1/DocsResource.php
+++ src/RubedoDoc/Rest/V1/DocsResource.php
@@ -1,5 +1,32 @@
 <?php
+namespace RubedoDoc\Rest\V1;
+
+use RubedoAPI\Entities\API\Definition\FilterDefinitionEntity;
+use RubedoAPI\Entities\API\Definition\VerbDefinitionEntity;
+use RubedoAPI\Rest\V1\AbstractResource;
 
 class DocsResource extends AbstractResource {
+    function __construct()
+    {
+        parent::__construct();
+        $this->define();
+    }
 
+    protected function define()
+    {
+        $this
+            ->definition
+            ->setName('Docs')
+            ->setDescription('Rubedo docs for your website ')
+            ->editVerb('get', function(VerbDefinitionEntity &$entity) {
+                $entity
+                    ->setDescription('Get full documentation')
+                    ->addOutputFilter(
+                        (new FilterDefinitionEntity())
+                            ->setDescription('Documentation entries')
+                            ->setKey('docs')
+                            ->setRequired()
+                    );
+            });
+    }
 } 
